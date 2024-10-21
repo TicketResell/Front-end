400f33eff8ad2aa0f145fb2d2286268b16b3cc53
HaiQuan21 <phamhaiquan22112004@gmail.com>
2024-10-21 16:14:20 +0700

    Chuc nang upload hinh anh va tin nhan trong chat

--- src/pages/Customer/Chat/index.js
+++ src/pages/Customer/Chat/index.js
@@ -10,24 +10,22 @@ import {
   Conversation,
   Avatar,
   ConversationHeader,
-  Search 
+  Search,
 } from "@chatscope/chat-ui-kit-react";
 import { Client } from "@stomp/stompjs";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 import api from "../../../config/axios";
 import uploadImgBB from "../../../config/imgBB";
 
-
 export default function Chat({ ticket, user }) {
   const [messages, setMessages] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [isBuyer, setIsBuyer] = useState(false); // Sử dụng isBuyer để xác định vai trò
   const [userName, setUserName] = useState("");
+  const [userAvatar, setUserAvatar] = useState("");
   const [connected, setConnected] = useState(false);
   const [activeIndex, setActiveIndex] = useState(null);
-  const [unreadCounts, setUnreadCounts] = useState(
-    [3,5,7,4,4,5]
-  );
+  const [unreadCounts, setUnreadCounts] = useState([3, 5, 7, 4, 4, 5]);
   const socket = useRef(null);
 
   const users = [
@@ -46,7 +44,18 @@ export default function Chat({ ticket, user }) {
     user.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  const handleSendMessage = (mess) => {
+  const getUserImageByID = async (id) => {
+    console.log("Id dùng để get userImage của người đối phương", id);
+    try {
+      const response = await api.post(`/accounts/get-avatar/${id}`); // Gửi id trong URL
+      console.log("UserImage : ", response.data);
+      setUserAvatar(response.data);
+    } catch (error) {
+      console.error("Error fetching user name", error);
+    }
+  };
+
+  const handleSendMessage = async (mess) => {
     console.log("User message:", mess);
     if (!connected) {
       console.error("WebSocket is not connected");
@@ -54,35 +63,38 @@ export default function Chat({ ticket, user }) {
     }
 
     const senderId = user.id;
-    const receiverId = isBuyer ? ticket.userID : messages[0]?.senderId; // Xác định receiverId
-    console.log("senderId",senderId)
-    console.log("messageContent",mess)
-    console.log("receiverId",receiverId)
-    console.log("Đối phương",userName)
+    console.log("M co phai Buyer ko", isBuyer);
+    const receiverId = isBuyer ? ticket.userID : messages[0]?.user1_id; // Xác định receiverId
+    console.log("senderId", senderId);
+    console.log("messageContent", mess);
+    console.log("receiverId", receiverId);
+    console.log("Đối phương", userName);
     if (!senderId || !mess.trim() || !receiverId) {
-      toast.error(
-        "Please enter message content.",
-        {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "light",
-          transition: Bounce,
-        }
-      );
+      toast.error("Please enter message content.", {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
       return;
     }
+    //Lấy hình ảnh của người gửi hiện tại
+    const response = await api.post(`/accounts/get-avatar/${senderId}`);
+    const imageSender = response.data;
+    console.log("Image Sender", imageSender);
 
     const messageToSend = JSON.stringify({
-      user1_id : senderId,
-      user2_id : receiverId,
+      user1_id: senderId,
+      user2_id: receiverId,
       messageContent: mess,
-      messageType:'text',
-      user2Name : userName,
+      messageType: "text",
+      user2Name: userName,
+      user1_avatar: imageSender,
     });
 
     console.log("Sending message:", messageToSend);
@@ -93,44 +105,54 @@ export default function Chat({ ticket, user }) {
     });
   };
 
-  const handleAttachFile = () =>{
-  const input = document.createElement('input');
-  input.type = 'file';
-  input.accept = 'image/*';
-  input.onchange = (e) => {
-    const img = e.target.files[0];
-    if (img) {
-      const imageUrl = uploadImgBB(img);
-      const imageMessage = {
-        messageContent: imageUrl, // Hiển thị hình ảnh trong tin nhắn
-        senderId: user.id,
-        timestamp: new Date().toISOString(),
-        type: 'image', // Đánh dấu kiểu tin nhắn là file
-      };
-      //Thên hình ảnh vào messagelist
-      console.log("Image :",img);
-      setMessages((prevMessages) => [...prevMessages, imageMessage]);
-
-      // Gửi file qua WebSocket nếu cần thiết (chỉ nếu bạn muốn)
-      const senderId = user.id;
-      const receiverId = isBuyer ? ticket.userID : messages[0]?.senderId; // Xác định receiverId
-
-      const messageToSend = JSON.stringify({
-        user1_id : senderId,
-        user2_id : receiverId,
-        messageContent: imageUrl,
-        messageType: 'image',
-        user2Name : userName,
-      });
-
-      socket.current.publish({
-        destination: "/app/sendMessage",
-        body: messageToSend,
-      });
+  const reconnectWebSocket = () => {
+    if (!socket.current || !socket.current.connected) {
+      connectWebSocket(); // Gọi lại hàm connectWebSocket
     }
   };
-  input.click();
-  }
+
+  const handleAttachFile = async () => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = "image/*";
+
+    input.onchange = async (e) => {
+      const img = e.target.files[0];
+      if (img) {
+        const imageUrlArray = await uploadImgBB([img]);
+        const imageUrl = imageUrlArray[0];
+
+        console.log("Hình ảnh Imgbb:", imageUrl);
+
+        const senderId = user.id;
+        const receiverId = isBuyer ? ticket.userID : messages[0]?.user1_id;
+        const response = await api.post(`/accounts/get-avatar/${senderId}`);
+        const imageSender = response.data;
+
+        const messageToSend = JSON.stringify({
+          user1_id: senderId,
+          user2_id: receiverId,
+          messageContent: imageUrl,
+          messageType: "image",
+          user2Name: userName,
+          user1_avatar: imageSender,
+        });
+
+        reconnectWebSocket(); // Gọi hàm để kết nối lại khi bị mất do chờ hình ảnh lên imgBB quá lâu
+        console.log("Sending message image:", messageToSend);
+        if (socket.current && socket.current.connected) {
+          socket.current.publish({
+            destination: "/app/sendMessage",
+            body: messageToSend,
+          });
+        } else {
+          console.error("STOMP connection is not established");
+        }
+      }
+    };
+
+    input.click();
+  };
 
   const connectWebSocket = () => {
     socket.current = new Client({
@@ -140,18 +162,23 @@ export default function Chat({ ticket, user }) {
         console.log("WebSocket connected");
         setConnected(true);
 
-        socket.current.subscribe("/topic/messages", (message) => {
-          console.log("Message Body JSON",message)
-          const msg = JSON.parse(message.body);
-          console.log("Message được trả về",msg);
-          setMessages((prevMessages) => [...prevMessages, msg]);
-        });
+        // Kiểm tra kết nối trước khi subscribe
+        if (socket.current && socket.current.connected) {
+          socket.current.subscribe("/topic/messages", (message) => {
+            console.log("Message Body JSON", message);
+            const msg = JSON.parse(message.body);
+            console.log("Message được trả về", msg);
+            setMessages((prevMessages) => [...prevMessages, msg]);
+          });
 
-        socket.current.subscribe("/topic/history", (history) => {
-          const chatHistory = JSON.parse(history.body);
-          console.log("Chat History",chatHistory)
-          setMessages(chatHistory);
-        });
+          socket.current.subscribe("/topic/history", (history) => {
+            const chatHistory = JSON.parse(history.body);
+            console.log("Chat History", chatHistory);
+            setMessages(chatHistory);
+          });
+        } else {
+          console.error("STOMP connection is not established");
+        }
       },
       onDisconnect: () => {
         console.log("WebSocket disconnected");
@@ -164,13 +191,11 @@ export default function Chat({ ticket, user }) {
 
     socket.current.activate();
   };
-
+  //Lấy tên người dùng bằng id
   const getUserNameByID = async (id) => {
     console.log("Id dùng để get userName của người đối phương", id);
     try {
-      const response = await api.post(`/accounts/hidden-search-profile/${id}`); // Gửi id trong URL
-      console.log("UserName : ",response.data);
-      console.log("Tên người bán",response.data)
+      const response = await api.post(`/accounts/hidden-search-profile/${id}`); 
       setUserName(response.data);
     } catch (error) {
       console.error("Error fetching user name", error);
@@ -180,13 +205,15 @@ export default function Chat({ ticket, user }) {
   useEffect(() => {
     const determineRole = () => {
       if (ticket && ticket.userID !== user.id) {
-        console.log("Ticket UserID Determinerole",ticket.userID);
-        console.log("UserID Login Determinerole",user.id)
+        console.log("Ticket UserID Determinerole", ticket.userID);
+        console.log("UserID Login Determinerole", user.id);
         setIsBuyer(true);
         getUserNameByID(ticket.userID); // Người mua get user của người bán
+        getUserImageByID(ticket.userID);
       } else {
         setIsBuyer(false);
-        getUserNameByID(messages[0]?.senderId); // Người bán get user của người mua
+        getUserNameByID(messages[0]?.user1_id); // Người bán get user của người mua
+        getUserImageByID(messages[0]?.user1_id);
       }
     };
 
@@ -200,72 +227,99 @@ export default function Chat({ ticket, user }) {
     };
   }, [messages]); // Khi danh sách tin nhắn thay đổi thì kiểm tra lại vai trò
 
-        // Hàm xử lý khi click vào một Conversation
+  // Hàm xử lý khi click vào một Conversation
   const handleChatClick = (index) => {
     setActiveIndex(index); // Cập nhật conversation đang active
-    setUnreadCounts((prevUnreadCounts) => 
-      prevUnreadCounts.map((count, i) => (i === index ? 0 : count)) // Đặt unreadCnt về 0 cho conversation được click
+    setUnreadCounts(
+      (prevUnreadCounts) =>
+        prevUnreadCounts.map((count, i) => (i === index ? 0 : count)) // Đặt unreadCnt về 0 cho conversation được click
     );
   };
 
   return (
-    <MainContainer >
-    <ToastContainer />
-    <Sidebar position="left">
-    <Search placeholder="Search..."
+    <MainContainer>
+      <ToastContainer />
+      <Sidebar position="left">
+        <Search
+          placeholder="Search..."
           value={searchTerm}
           onChange={(e) => setSearchTerm(e)}
-          onClearClick={() => setSearchTerm("")} />
-      <ConversationList>
-        {filteredUsers.map((user, index) => (
-          <Conversation key={index} name={user.name} lastSenderName={user.name} info = {user.lastMessage} active = {activeIndex === index}  unreadCnt={unreadCounts[index]} lastActivityTime="43 min" onClick={() => handleChatClick(index)} >
-            <Avatar src='https://i.ibb.co/wpnnQ3Q/a882ecea-527f-4cd7-b2c4-2587a2d10e23.jpg'/> 
-            <p>{user.lastMessage}</p>
-          </Conversation>
-        ))}
-      </ConversationList>
-    </Sidebar>
+          onClearClick={() => setSearchTerm("")}
+        />
+        <ConversationList>
+          {filteredUsers.map((user, index) => (
+            <Conversation
+              key={index}
+              name={user.name}
+              lastSenderName={user.name}
+              info={user.lastMessage}
+              active={activeIndex === index}
+              unreadCnt={unreadCounts[index]}
+              lastActivityTime="43 min"
+              onClick={() => handleChatClick(index)}
+            >
+              <Avatar src="https://i.ibb.co/wpnnQ3Q/a882ecea-527f-4cd7-b2c4-2587a2d10e23.jpg" />
+              <p>{user.lastMessage}</p>
+            </Conversation>
+          ))}
+        </ConversationList>
+      </Sidebar>
       <ChatContainer>
-        
-      <ConversationHeader>
-        <Avatar src="https://i.ibb.co/wpnnQ3Q/a882ecea-527f-4cd7-b2c4-2587a2d10e23.jpg" status="dnd"/>
-        <ConversationHeader.Content userName={userName}></ConversationHeader.Content>
-      </ConversationHeader>
-      <MessageList>
-  {messages.map((message, index) => (
-    <Message
-      key={index}
-      model={{
-        message: message.messageContent,
-        direction: message.user1_id === user.id ? "outgoing" : "incoming",
-        type: message.messageType,
-        sentTime: message.timestamp,
-        sender: message.user1_id === user.id ? "You" : userName, // Thay thế cho Header
-      }}
-      avatarSpacer={true}
-    >
-      {/* Avatar cho người gửi */}
-      <Avatar src={message.user1_id === user.id ? user.userImage : messages[0]?.user1_id.userImage} />
+        <ConversationHeader>
+          {/* Avatar cho người nhận */}
+          <Avatar src={userAvatar} status="dnd" />
+          <ConversationHeader.Content
+            userName={userName}
+          ></ConversationHeader.Content>
+        </ConversationHeader>
+        <MessageList>
+          {messages.map((message, index) => (
+            <Message
+              key={index}
+              model={{
+                message:
+                  message.messageType === "text" ? message.messageContent : "",
+                direction:
+                  message.user1_id === user.id ? "outgoing" : "incoming",
+                type: message.messageType,
+                sentTime: message.timestamp,
+                sender: message.user1_id === user.id ? "You" : userName,
+              }}
+              avatarSpacer={true}
+            >
+              {/* Hiển thị nội dung tin nhắn */}
+              {message.messageType === "text" ? (
+                <Message.CustomContent>
+                  {message.messageContent} {/* Hiển thị văn bản */}
+                </Message.CustomContent>
+              ) : message.messageType === "image" ? (
+                <Message.CustomContent>
+                  <img
+                    src={message.messageContent}
+                    alt="Message content"
+                    style={{
+                      maxWidth: "400px",
+                      maxHeight: "250px",
+                      borderRadius: "8px",
+                      display: "block",
+                      margin: "0 auto",
+                      objectFit: "cover",
+                    }}
+                  />
+                </Message.CustomContent>
+              ) : null}
+              {/* Avatar cho người gửi */}
+              <Avatar src={message.user1_avatar} />
+            </Message>
+          ))}
+        </MessageList>
 
-      {/* Nếu là tin nhắn hình ảnh */}
-      {message.chatType === 'image' && (
-        <Message.ImageContent src={message.messageContent} alt="attached" style={{ maxWidth: "200px", borderRadius: "8px" }} />
-      )}
-
-      {/* Nếu là tin nhắn text */}
-      {message.chatType === 'text' && (
-        <Message.CustomContent>
-          <div>{message.messageContent}</div>
-          <div style={{ fontSize: '12px', color: 'gray' }}>{message.user1_id === user.id ? "Sent" : "Received"}</div> {/* Thay cho Footer */}
-        </Message.CustomContent>
-      )}
-
-    </Message>
-  ))}
-</MessageList>
-
-      <MessageInput placeholder="Type message here" onSend={handleSendMessage} onAttachClick={handleAttachFile} />
-    </ChatContainer>
-  </MainContainer>
+        <MessageInput
+          placeholder="Type message here"
+          onSend={handleSendMessage}
+          onAttachClick={handleAttachFile}
+        />
+      </ChatContainer>
+    </MainContainer>
   );
 }
